ac0b531e55c8c34fa62b165e058d0360a22ebffe
MickaelRigault <mick.rigault@gmail.com>
2018-11-26 11:03:38 +0100

    generic info

---
+++ config.js
@@ -0,0 +1,5 @@
+
+var ZTFDATA = {"1": {"color": "rgb(61, 163, 73)", "name": "ztf-g"},
+               "2": {"color": "rgb(194, 0, 26)",  "name": "ztf-r"},
+               "3": {"color": "rgb(244, 156, 16)", "name": "ztf-i"},
+    };
