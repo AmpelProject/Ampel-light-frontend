99810ebc6548dc74d446b2fb476ff8df921fc677
nmiranda <nicolas.miranda@hu-berlin.de>
2018-12-06 13:42:03 +0100

    Times from unix format to dates

--- toolbox.js
+++ toolbox.js
@@ -6,3 +6,15 @@ function julianDateToDate(jd) {
 function argMax(array) {
     return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
 }
+
+function unixTimestampToDate(unix_timestamp) {
+    var ts = new Date(unix_timestamp * 1000);
+    var year = ts.getFullYear();
+    var month = ts.getMonth() + 1;
+    var day = ts.getDate();
+    var hour = ts.getHours() < 10 ? '0' + ts.getHours() : ts.getHours();
+    var min = ts.getMinutes() < 10 ? '0' + ts.getMinutes() : ts.getMinutes();
+    var sec = ts.getSeconds() < 10 ? '0' + ts.getSeconds() : ts.getSeconds();
+    var date = day + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec;
+    return date;
+}
