15a510611a87f7c15565a93bf4bc6dfbdf6e18e2
Jakob van Santen <jakob.van.santen@desy.de>
2018-12-06 22:11:23 +0100

    Add FITS viewer when you click on points

--- fits/fitsviewer.js
+++ fits/fitsviewer.js
@@ -56,7 +56,7 @@ function FitsViewer(input){
 			//document.getElementById('z').value = 0;
 
 		}).bind("load",{me:this, key:k},function(e){
-			var canvas = e.data.me.canvas + "." + e.data.key;
+			var canvas = e.data.me.canvas + "_" + e.data.key;
 			console.log("loading "+canvas);
 			this.draw(canvas);
 			$('#'+e.data.me.id+' .loader').hide();
@@ -66,90 +66,44 @@ function FitsViewer(input){
 
 	// Build the viewer here
 	var html = '';
-	html += 'Stretch function: <select class="stretch" name="stretch">';
-	for(i = 0 ; i < this.stretches.length ; i++){
-		html += '<option'+(this.stretches[i]== this.fits.stretch ? ' selected="selected"' : '')+'>'+this.stretches[i]+'</option>';
-	}
-	html += '</select>';
-	html += 'Color scheme: <select class="scale" name="scale">';
-	for(i = 0 ; i < this.colors.length ; i++){
-		html += '<option'+(this.colors[i]== this.fits.color ? ' selected="selected"' : '')+'>'+this.colors[i]+'</option>';
-	}
-	html += '</select>';
+
+  html += '<div class="row">';
 	for (var k in this.fits) {
-		html += '<br /><canvas id="'+this.canvas+'.'+k+'"></canvas>';
+		html += '<div id="col-md-2 col-sm-2"><canvas id="'+this.canvas+'_'+k+'"></canvas></div>';
 	}
-	html += '<ul id="list"></ul>';
+	html += '</div>';
+	html += '<div class="row">';
+  
+  html += '<div class="form-group"><label for="FITSViewerStretch">Stretch</label>'
+  html += '<select class="form-control form-control-sm" name="stretch" id="FITSViewerStretch">';
+  for(i = 0 ; i < this.stretches.length ; i++){
+    html += '<option'+(this.stretches[i]== this.fits.stretch ? ' selected="selected"' : '')+'>'+this.stretches[i]+'</option>';
+  }
+  html += '</select></div>';
+  html += '<div class="form-group"><label for="FITSViewerScale">Color</label>';
+  html += '<select class="form-control form-control-sm" name="scale" id="FITSViewerScale">';
+  for(i = 0 ; i < this.colors.length ; i++){
+    html += '<option'+(this.colors[i]== this.fits.color ? ' selected="selected"' : '')+'>'+this.colors[i]+'</option>';
+  }
+  html += '</select></div>';
+	html += '</div>';
 
 	$('#'+this.id+'').html(html);
-	
-	this.imageUnderlay('Try dragging and dropping a FITS file into this box');
 
 	// Bind events
  	$('#'+this.id+' select.file').bind('change',{me:this}, function(e){
  		e.data.me.fits.load($(this).val());
  	});
- 	$('#'+this.id+' select.stretch').bind('change',{me:this}, function(e){
+ 	$('#'+this.id+' select[name="stretch"]').bind('change',{me:this}, function(e){
 		for (var k in e.data.me.fits) {
 	 		e.data.me.fits[k].update($(this).val());
 		}
  	});
- 	$('#'+this.id+' select.scale').bind('change',{me:this}, function(e){
+ 	$('#'+this.id+' select[name="scale"]').bind('change',{me:this}, function(e){
 		for (var k in e.data.me.fits) {
 	 		e.data.me.fits[k].update({color:$(this).val()});
 		}
  	});
-
-	if(this.capability.file){
-		// We can use the HTML5 file capabilities. Yay!
-		
-		// Setup the drag-n-drop listeners.
-		$('#'+this.canvas).bind("dragenter",function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-			$(this).addClass("dragover");
-		}).bind("dragover",function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-		}).bind("dragleave",function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-			$(this).removeClass("dragover");
-		}).bind("drop",{me:this},function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-			$(this).removeClass("dragover");
-
-			var files = e.originalEvent.dataTransfer.files; // FileList object.
-
-			// files is a FileList of File objects. List some properties.
-			var output = [];
-
-			output.push('<li><strong>', files[0].name, '</strong> (', files[0].type || 'n/a', ') - ',
-			files[0].size, ' bytes','</li>');
-			//files[0].lastModifiedDate.toLocaleDateString(), '</li>');
-
-			e.data.me.processFile(files[0].name)
-
-			document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-
-		});
-	} else {
-		$('#'+this.id).prepend('<p>The File APIs are not fully supported in this browser. You are missing out on the awesome. :-(</p>');
-	}
-
-
-/*
-	Frame <button onClick="fits.update({index:--(document.getElementById('z').value)})">&lt;</button>
-	<input id="z" name="z" value="0" size=3 onChange="fits.update({index:this.value})">
-	<button onClick="fits.update({index:++(document.getElementById('z').value)})">&gt;</button> of <span id=depth></span>. Format: <span id=bitpix></span>.
-	<span id="status"></span>
-	<br>
-*/
-
-
-	// Load an initial FITS file
-	if(this.file) this.fits.load(this.file);
 }
 
 
--- toolbox.js
+++ toolbox.js
@@ -14,3 +14,7 @@ function julianIntToDate(n) {
 
     return new Date(Y,M,D);
 }
+
+function argMax(array) {
+    return array.map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
