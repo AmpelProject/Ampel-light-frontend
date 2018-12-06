63dc15b19a663f4ea679cc058e9ada5972a9f2c0
nmiranda <nicolas.miranda@hu-berlin.de>
2018-12-06 13:42:03 +0100

    Times from unix format to dates

--- toolbox.js
+++ toolbox.js
@@ -14,3 +14,15 @@ function julianIntToDate(n) {
 
     return new Date(Y,M,D);
 }
+
+function unixTimestampToDate(unix_timestamp) {
+    var ts = new Date(unix_timestamp * 1000);
+    var year = ts.getFullYear();
+    var month = ts.getMonth() + 1;
+    var day = ts.getDate();
+    var hour = ts.getHours() < 10 ? '0' + ts.getHours() : ts.getHours();
+    var min = ts.getMinutes() < 10 ? '0' + ts.getMinutes() : ts.getMinutes();
+    var sec = ts.getSeconds() < 10 ? '0' + ts.getSeconds() : ts.getSeconds();
+    var date = day + '.' + month + '.' + year + ' ' + hour + ':' + min + ':' + sec;
+    return date;
+}
